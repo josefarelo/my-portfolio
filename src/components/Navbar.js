deb36748d558cdff8716e868881582777885d0cb
joseF <jose.farelo@hotmail.com>
2024-06-01 14:52:35 -0300

    Ajustes de color de background al usar la funcion 'theme'.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,19 @@ import './Navbar.scss';
 const Navbar = ({ toggleTheme, toggleLanguage }) => {
     return (
         <nav className="navbar">
-            <h1>My Portfolio</h1>
+            <h1>José Farelo</h1>
             <div className="navbar-links">
                 <Link to="about" smooth={true} duration={600}>
-                    About
+                About
                 </Link>
                 <Link to="skills" smooth={true} duration={600}>
-                    Skills
+                Skills
                 </Link>
                 <Link to="projects" smooth={true} duration={600}>
-                    Projects
+                Projects
                 </Link>
                 <Link to="contact" smooth={true} duration={600}>
-                    Contact
+                Contact
                 </Link>
             </div>
             <div className="navbar-actions">
@@ -29,3 +29,4 @@ const Navbar = ({ toggleTheme, toggleLanguage }) => {
 };
 
 export default Navbar;
+
